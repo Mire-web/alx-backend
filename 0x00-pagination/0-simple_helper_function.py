16928860c59698bdfbf6aa7a5a0007f12e7cc0eb
mire-web <amahe8664@gmail.com>
2024-09-02 01:06:09 +0100

    simple

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,8 +7,4 @@ from typing import Tuple
 
 def index_range(page: int = 1, page_size: int = 15) -> Tuple[int, int]:
     """Return the start and end Index for a pagination"""
-    if page == 1:
-        return (0, page_size)
-    start_index = (page * 10)
-    end_index = start_index + page_size
-    return (start_index, end_index)
+    return ((page - 1) * page_size, page_size * page)
