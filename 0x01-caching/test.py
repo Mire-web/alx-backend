e92fa50f819fa74ce03bbf4d5ca04d4e236d2f9a
mire-web <amahe8664@gmail.com>
2024-09-06 14:49:32 +0100

    lifo

--- 0x01-caching/test.py
+++ 0x01-caching/test.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""
-Test
-"""
-import sys
+""" 2-main """
+LIFOCache = __import__('2-lifo_cache').LIFOCache
 
-try:
-    FIFOCache = __import__('1-fifo_cache').FIFOCache
-
-    my_cache = FIFOCache()
-    my_cache.print_cache()
-    my_cache.put("test1", "myValue")
-    my_cache.print_cache()
-    if my_cache.get(None) is not None:
-        print("get must return None if the key is None")
-    else:
-        print("OK")
-    if my_cache.get("test2") is not None:
-        print("test2 doesn't exist in the cache, get must return None")
-    else:
-        print("OK")
-except:
-    print(sys.exc_info()[1])
+my_cache = LIFOCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+my_cache.put("F", "Mission")
+my_cache.print_cache()
+my_cache.put("G", "San Francisco")
+my_cache.print_cache()
