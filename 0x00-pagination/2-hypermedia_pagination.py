d80ff1ab6277bdc6f26ad0621050c1e710c2e032
mire-web <amahe8664@gmail.com>
2024-09-06 13:16:16 +0100

    hyper

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -30,6 +30,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get paginated Page"""
         assert type(page) == int and page > 0
         assert type(page_size) == int and page_size > 0
         start_index, end_index = index_range(page, page_size)
